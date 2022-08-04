d663f8096768c7d6b7932ce9832dae3bfac50441
AndyRama <andyramaroson@gmail.com>
2022-08-04 16:49:35 +0200

    [test] add beforeach in bills.js and begin bills Unit test suites new page

--- Billed-app-FR-Front/src/__tests__/Bills.js
+++ Billed-app-FR-Front/src/__tests__/Bills.js
@@ -11,30 +11,37 @@ import {localStorageMock} from "../__mocks__/localStorage.js";
 
 import router from "../app/Router.js";
 
-describe("Given I am connected as an employee", () => {
-  describe("When I am on Bills Page", () => {
-    test("Then bill icon in vertical layout should be highlighted", async () => {
+beforeEach(()=> {
+  // On simule une connection page employee en paramétrant le stockage local
+  Object.defineProperty(window, 'localStorage', { value: localStorageMock })
+  window.localStorage.setItem('user', JSON.stringify({
+    type: 'Employee'
+  }))
+})
+describe('Bills Unit test suites', () => {
+  
+  describe("Given I am connected as an employee", () => {
+    describe("When I am on Bills Page", () => {
+      test("Then bill icon in vertical layout should be highlighted", async () => {
+        const root = document.createElement("div")
+        root.setAttribute("id", "root")
+        document.body.append(root)
+        router()
+        window.onNavigate(ROUTES_PATH.Bills)
+        await waitFor(() => screen.getByTestId('icon-window'))
+        const windowIcon = screen.getByTestId('icon-window')
+        expect(windowIcon).toHaveClass('active-icon')
+      })
 
-      Object.defineProperty(window, 'localStorage', { value: localStorageMock })
-      window.localStorage.setItem('user', JSON.stringify({
-        type: 'Employee'
-      }))
-      const root = document.createElement("div")
-      root.setAttribute("id", "root")
-      document.body.append(root)
-      router()
-      window.onNavigate(ROUTES_PATH.Bills)
-      await waitFor(() => screen.getByTestId('icon-window'))
-      const windowIcon = screen.getByTestId('icon-window')
-      //to-do write expect expression
-      expect(windowIcon).toHaveClass('active-icon')
+      test("Then bills should be ordered from earliest to latest", () => {
+        document.body.innerHTML = BillsUI({ data: bills })
+        const dates = screen.getAllByText(/^(19|20)\d\d[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])$/i).map(a => a.innerHTML)
+        const antiChrono = (a, b) => ((a < b) ? 1 : +1)
+        const datesSorted = [...dates].sort(antiChrono)
+        expect(dates).toEqual(datesSorted)
+      })
     })
-    test("Then bills should be ordered from earliest to latest", () => {
-      document.body.innerHTML = BillsUI({ data: bills })
-      const dates = screen.getAllByText(/^(19|20)\d\d[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])$/i).map(a => a.innerHTML)
-      const antiChrono = (a, b) => ((a < b) ? 1 : +1)
-      const datesSorted = [...dates].sort(antiChrono)
-      expect(dates).toEqual(datesSorted)
-    })
+    describe("When")
   })
+  
 })
