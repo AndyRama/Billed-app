b0019fa62957ecf5018d4ab31749f178c26d5f73
AndyRama <andyramaroson@gmail.com>
2022-09-11 23:13:28 +0200

    [ehancement] add Test Unit for form submit form NewBill

--- Billed-app-FR-Front/src/__tests__/NewBill.js
+++ Billed-app-FR-Front/src/__tests__/NewBill.js
@@ -2,7 +2,7 @@
  * @jest-environment jsdom
  */
 
-import { fireEvent, screen } from "@testing-library/dom"
+import { fireEvent, getByTestId, screen } from "@testing-library/dom"
 import NewBillUI from "../views/NewBillUI.js"
 import NewBill from "../containers/NewBill.js"
 import { localStorageMock } from"../__mocks__/localStorage.js"
@@ -25,56 +25,99 @@ beforeEach(()=> {
   document.body.innerHTML = NewBillUI()
 })
 
-describe("NewBills Unit test suites" ,() => {
+//Test for NewBill Submit form 
+describe("NewBill Unit test suites", () => {
   describe("Given I am connected as an employee", () => {
-    describe("When I am on NewBill Page", () => {
+    describe("When I am on NewBill page", () => {
+      test("Then Isubmit a compete NewBills form and redirected on bill, methode Post", async() => {
+      // route
+      document.body.innerHTML = `<div id="root"></div>`; 
+      router()
+      window.onNavigate(ROUTES_PATH.NewBill)
+      // value for Expense-name
+      const expenseName = screen.getByTestId("expense-name")
+      expenseName.value ='Hôtel'
+      // value for Datepicker
+      const datePicker = screen .getByTestId("datepicker")
+      datePicker.vaue = "2022-07-25"
+      // value for Amount
+      const amount = screen.getByTestId("amount")
+      amount.value = "240"
+      // value for TVA
+      const tva = screen.getByTestId("vat")
+      tva.value = "30"
+      // value for Pct
+      const pct = screen.getByTestId("pct")
+      pct.value = "20"
+      // File and fireEvent
+      const file = screen.getByTestId("file")
+      fireEvent.change(file, {
+        target: {
+          files: [new File(['(⌐□_□)'], 'chucknorris.png', { type: 'image/png'})],
+        },
+      })
+      // Form Submission
+      const formSubmission = screen.getByTestId('form-new-bill')
+      const newBillEmulation = new NewBill( { document, onNavigate, store: mockStore, localStorage: window.localStorage })
+      const handleSubmit = jest.fn((e) => { newBillEmulation.handleSubmit(e)})
+      // addEventListener on form
+      formSubmission.addEventListener('submit', handleSubmit)
+      fireEvent.submit(formSubmission)
+      expect(handleSubmit).toHaveBeenCalled() 
+      })
+    })
+  })
+})
+
+describe("NewBill Unit test suites", () => {
+  describe("Given I am connected as an employee", () => {
+    describe("When I am on NewBill page", () => {
       describe("When I try to load a picture", () => {
         test("Then file sould be an picture", () => {
           // Récupération input file
-          const newFile = screen.getByTestId("file")
-          // Récupération de nouvelle instance NewBill
-          const onNavigate = (pathname) => document.body.innerHTML = ROUTES( { pathname })
-          const newBillEmulation = new NewBill({ document, onNavigate, store:mockStore, localStorage:window.localStorage })
+          const newFile = screen.getByTestId('file')
+          // Récupération de nouvelle instance de NewBill
+          const onNavigate = (pathname) => document.body.innerHTML = ROUTES({ pathname })
+          const newBillEmulation = new NewBill({ document, onNavigate, store: mockStore, localStorage: window.localStorage })
           const handleChangeFile = jest.fn((e) => newBillEmulation.handleChangeFile(e))
-          // AddEventListener handleChangeFile
-          newFile.addEventListener('change', handleChangeFile)
+          // addEventListener handleChangeFile
+          newFile.addEventListener("change", handleChangeFile)
           userEvent.click(newFile)
-          // Vérifier si le fichier est une image
+          //  Vérifié si le fichier est bien une image
           fireEvent.change(newFile, {
             target: {
-              files:[ new File(['(⌐□_□)'], 'chucknorris.png', { type: 'image/png' })],
+              files: [new File(['(⌐□_□)'], 'chucknorris.png', { type: 'image/png'})],
             },
           })
-          expect(newFile.file[0].type).toMatch(/(image\/jpg) | (image\/jpeg) | (image\/png)/gm)
+          expect(newFile.files[0].type).toMatch(/(image\/jpg)|(image\/jpeg)|(image\/png)/gm)
         })
 
-        test("then file sould be not an image ", () => {
+        test("then file should not be an image", ( )=> {
           const jsdomAlert = window.alert;
           window.alert = () => { };
 
-           // Récupération input file
-           const newFile = screen.getByTestId("file")
-           // Récupération de nouvelle instance NewBill
-           const onNavigate = (pathname) => document.body.innerHTML = ROUTES( { pathname })
-           const newBillEmulation = new NewBill({ document, onNavigate, store:mockStore, localStorage:window.localStorage })
-           const handleChangeFile = jest.fn((e) => newBillEmulation.handleChangeFile(e))
-           // AddEventListener handleChangeFile
-           newFile.addEventListener('change', handleChangeFile)
-           userEvent.click(newFile)
-           // Vérifier si le fichier est une image
-           fireEvent.change(newFile, {
-             target: {
-               files:[ new File(['(⌐□_□)'], 'chucknorris.txt', { type: 'text/plain' })],
-             },
-           })
-           expect(newFile.file[0].type).not.toMatch(/(image\/jpg) | (image\/jpeg) | (image\/png)/gm)
-           // Restore the jsdom alert 
-           window.alert = jsdomAlert;
+          // Récupération input file
+          const newFile = screen.getByTestId('file')
+          // Récupération de nouvelle instance de NewBill
+          const onNavigate = (pathname) => document.body.innerHTML = ROUTES({ pathname })
+          const newBillEmulation = new NewBill({ document, onNavigate, store: mockStore, localStorage: window.localStorage })
+          const handleChangeFile = jest.fn((e) => newBillEmulation.handleChangeFile(e))
+          // addEventListener handleChangeFile
+          newFile.addEventListener("change", handleChangeFile)
+          userEvent.click(newFile)
+          //  Vérifié si le fichier est bien une image
+          fireEvent.change(newFile, {
+            target: {
+              files: [new File(['(⌐□_□)'], 'chucknorris.txt', {type: 'text/plain'})],
+            },
+          })
+          expect(newFile.files[0].type).not.toMatch(/(image\/jpg)|(image\/jpeg)|(image\/png)/gm)
+          window.alert = jsdomAlert;
+          // restore the jsdom alert
         })
       })
     })
   })
 })
 
-// Test Newbilll submit form
 // Test d'intégration POST
