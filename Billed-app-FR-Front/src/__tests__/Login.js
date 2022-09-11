47d3ae418b09e34a5083de0c331cf86657a9990f
AndyRama <andyramaroson@gmail.com>
2022-09-11 15:43:49 +0200

    [enhancement] add Test Unit suite for NewBill

--- Billed-app-FR-Front/src/__tests__/Login.js
+++ Billed-app-FR-Front/src/__tests__/Login.js
@@ -6,6 +6,8 @@ import LoginUI from "../views/LoginUI";
 import Login from "../containers/Login.js";
 import { ROUTES } from "../constants/routes";
 import { fireEvent, screen } from "@testing-library/dom";
+import mockStore from "../__mocks__/store"
+
 
 describe("Given that I am a user on login page", () => {
   describe("When I do not fill fields and I click on employee button Login In", () => {
@@ -225,6 +227,106 @@ describe("Given that I am a user on login page", () => {
 
     test("It should renders HR dashboard page", () => {
       expect(screen.queryByText("Validations")).toBeTruthy();
+    });
+
+
+    describe("When I create a user with a valid email and bad password and I click on admin button Login In", () => {
+      test("Then it should renders message error Login page", () => {
+        document.body.innerHTML = LoginUI();
+        const inputData = {
+          email: "andy@gmail.com",
+          password: "azerty",
+        };
+
+        const inputEmailUser = screen.getByTestId("employee-email-input");
+        fireEvent.change(inputEmailUser, { target: { value: inputData.email } });
+        expect(inputEmailUser.value).toBe(inputData.email);
+
+        const inputPasswordUser = screen.getByTestId("employee-password-input");
+        fireEvent.change(inputPasswordUser, {
+          target: { value: inputData.password },
+        });
+        expect(inputPasswordUser.value).toBe(inputData.password);
+
+        const form = screen.getByTestId("form-employee");
+
+        // localStorage should be populated with form data
+        Object.defineProperty(window, "localStorage", {
+          value: {
+            getItem: jest.fn(() => null),
+            setItem: jest.fn(() => null),
+          },
+          writable: true,
+        });
+
+        // we have to mock navigation to test it
+        const onNavigate = (pathname) => {
+          document.body.innerHTML = ROUTES({ pathname });
+        };
+
+        let PREVIOUS_LOCATION = "";
+
+        const store = jest.fn();
+
+        const login = new Login({
+          document,
+          localStorage: window.localStorage,
+          onNavigate,
+          PREVIOUS_LOCATION,
+          store,
+        });
+
+        const handleSubmit = jest.fn(login.handleSubmitEmployee);
+        login.login = jest.fn().mockResolvedValue({});
+        form.addEventListener("submit", handleSubmit);
+        fireEvent.submit(form);
+        expect(handleSubmit).toHaveBeenCalled();
+          const user = JSON.stringify({
+            type: "Employee",
+            email: inputData.email,
+            password: inputData.password,
+            status: "connected",
+          });
+          const storeCreateUser = jest.fn();
+          const createUser = new Login({
+            document,
+            localStorage: window.localStorage,
+            onNavigate,
+            PREVIOUS_LOCATION,
+            storeCreateUser,
+          });
+        createUser.createUser(user);
+      });
+    });
+  
+    // test d'intégration POST
+    describe('Given I am a user connected as Employee and I am on NewBill page', () => {
+      describe('When I submit the new bill', () => {
+        test('create a new bill from mock API POST', async () => {
+
+          const bill = [{
+            "id": "47qAXb6fIm2zOKkLzMro",
+            "vat": "80",
+            "fileUrl": "https://test.storage.tld/v0/b/billable-677b6.a…f-1.jpg?alt=media&token=c1640e12-a24b-4b11-ae52-529112e9602a",
+            "status": "pending",
+            "type": "Hôtel et logement",
+            "commentary": "séminaire billed",
+            "name": "encore",
+            "fileName": "preview-facture-free-201801-pdf-1.jpg",
+            "date": "2004-04-04",
+            "amount": 400,
+            "commentAdmin": "ok",
+            "email": "a@a",
+            "pct": 20
+          }]
+          const callStore = jest.spyOn(mockStore, 'bills');
+
+          mockStore.bills().create(bill);
+
+          expect(callStore).toHaveBeenCalled();
+
+        });
+      });
     });
   });
 });
--- Billed-app-FR-Front/src/__tests__/NewBill.js
+++ Billed-app-FR-Front/src/__tests__/NewBill.js
@@ -2,17 +2,79 @@
  * @jest-environment jsdom
  */
 
-import { screen } from "@testing-library/dom"
+import { fireEvent, screen } from "@testing-library/dom"
 import NewBillUI from "../views/NewBillUI.js"
 import NewBill from "../containers/NewBill.js"
+import { localStorageMock } from"../__mocks__/localStorage.js"
+import mockStore from "../__mocks__/store"
+import userEvent from "@testing-library/user-event"
+import router from "../app/Router"
+import { ROUTES, ROUTES_PATH } from "../constants/routes.js"
 
 
-describe("Given I am connected as an employee", () => {
-  describe("When I am on NewBill Page", () => {
-    test("Then ...", () => {
-      const html = NewBillUI()
-      document.body.innerHTML = html
-      //to-do write assertion
+jest.mock("../app/Store", () => mockStore)
+
+beforeEach(()=> {
+  // simulate the connection on the Employee page by setting the localStorage
+  Object.defineProperty(window, 'localStorage', { value: localStorageMock })
+  window.localStorage.setItem('user', JSON.stringify({
+    type: 'Employee',
+    email: 'employee@test.tld'
+  }))
+  // Afficher la page nouvelle note de frais
+  document.body.innerHTML = NewBillUI()
+})
+
+describe("NewBills Unit test suites" ,() => {
+  describe("Given I am connected as an employee", () => {
+    describe("When I am on NewBill Page", () => {
+      describe("When I try to load a picture", () => {
+        test("Then file sould be an picture", () => {
+          // Récupération input file
+          const newFile = screen.getByTestId("file")
+          // Récupération de nouvelle instance NewBill
+          const onNavigate = (pathname) => document.body.innerHTML = ROUTES( { pathname })
+          const newBillEmulation = new NewBill({ document, onNavigate, store:mockStore, localStorage:window.localStorage })
+          const handleChangeFile = jest.fn((e) => newBillEmulation.handleChangeFile(e))
+          // AddEventListener handleChangeFile
+          newFile.addEventListener('change', handleChangeFile)
+          userEvent.click(newFile)
+          // Vérifier si le fichier est une image
+          fireEvent.change(newFile, {
+            target: {
+              files:[ new File(['(⌐□_□)'], 'chucknorris.png', { type: 'image/png' })],
+            },
+          })
+          expect(newFile.file[0].type).toMatch(/(image\/jpg) | (image\/jpeg) | (image\/png)/gm)
+        })
+
+        test("then file sould be not an image ", () => {
+          const jsdomAlert = window.alert;
+          window.alert = () => { };
+
+           // Récupération input file
+           const newFile = screen.getByTestId("file")
+           // Récupération de nouvelle instance NewBill
+           const onNavigate = (pathname) => document.body.innerHTML = ROUTES( { pathname })
+           const newBillEmulation = new NewBill({ document, onNavigate, store:mockStore, localStorage:window.localStorage })
+           const handleChangeFile = jest.fn((e) => newBillEmulation.handleChangeFile(e))
+           // AddEventListener handleChangeFile
+           newFile.addEventListener('change', handleChangeFile)
+           userEvent.click(newFile)
+           // Vérifier si le fichier est une image
+           fireEvent.change(newFile, {
+             target: {
+               files:[ new File(['(⌐□_□)'], 'chucknorris.txt', { type: 'text/plain' })],
+             },
+           })
+           expect(newFile.file[0].type).not.toMatch(/(image\/jpg) | (image\/jpeg) | (image\/png)/gm)
+           // Restore the jsdom alert 
+           window.alert = jsdomAlert;
+        })
+      })
     })
   })
 })
+
+// Test Newbilll submit form
+// Test d'intégration POST
